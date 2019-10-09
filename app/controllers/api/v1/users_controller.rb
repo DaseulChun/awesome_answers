395d79bef6cd9f33cb06e6596767cfe6007cc4fd
DaseulChun <nandaseul@gmail.com>
2019-10-08 18:41:10 -0700

    fix bug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < Api::ApplicationController
   private
 
   def user_params
-    params.require(:user)
+    params
     .permit(
       :first_name, 
       :last_name, 
